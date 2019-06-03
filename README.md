fab9ebd04d0735490c967dbeb82a7a0516373fa4
susanwere <blue.were@yahoo.com>
2019-06-03 14:49:11 +0300

    chore(rspec/graphql): setup tests functionality - setup rspec - setup graphql [Finishes #166212916]

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Authors_Haven - A Social platform for the creative at heart.
-A social platform for the creative at heart
 
-## Vision
-Create a community of like minded authors to foster inspiration and innovation
-by leveraging the modern web.
+A social platform for the creative at heart	
+
+
+ ## Vision
+Create a community of like minded authors to foster inspiration and innovation by leveraging the modern web.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,15 +22,6 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
@@ -40,11 +31,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-
-  # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
